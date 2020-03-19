c0f86c169a7994f4bcc794bd77b15439d6fb4b8d
Justin Cook <justin.cook.dev@gmail.com>
2020-03-19 16:36:51 -0700

    Typing game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { lorem } from "faker";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'typing';
+  title = "typing game";
+
+  private _randomText = lorem.sentence().slice(0, -1);
+  private _text = "";
+
+  public get randomText(): string {
+    return this._randomText;
+  }
+
+  public get text(): string {
+    return this._text;
+  }
+
+  public set text(t: string) {
+    this._text = t;
+  }
+
+  public showSuccess(): boolean {
+    return (
+      this.text.length === this.randomText.length &&
+      this.text === this.randomText
+    );
+  }
+
+  public compare(randomTextChar: string, enteredChar: string): string {
+    return enteredChar
+      ? randomTextChar === enteredChar
+        ? "has-text-success"
+        : "has-text-danger"
+      : "";
+  }
 }
